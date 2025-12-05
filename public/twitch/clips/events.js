7ca0e4f54537daeab45bb77d23b12b6a1e6ce1db
Ponss17 <Ponss17@users.noreply.github.com>
2025-12-04 23:09:11 -0600

    Prueba de Flixeo

--- public/twitch/clips/events.js
+++ public/twitch/clips/events.js
@@ -1,5 +1,29 @@
 import { setupCopy } from '/js/copy.js';
-import { renderCommands } from './commands.js';
+
+function renderCommands(baseUrl, authCode, userId, token) {
+  console.log('Rendering commands:', { baseUrl, authCode, userId, token });
+  const authParam = authCode ? `auth=${authCode}` : `user_id=${userId}&token=${token}`;
+
+  const seCommand = `$(customapi ${baseUrl}/api/clips/create?${authParam}&channel=$(channel)&creator=\${user})`;
+  const streamElementsCommand = document.getElementById('streamElementsCommand');
+  if (streamElementsCommand) {
+    streamElementsCommand.textContent = seCommand;
+  } else {
+    console.error('Element streamElementsCommand not found');
+  }
+
+  const nbCommand = `$(urlfetch ${baseUrl}/api/clips/create?${authParam}&channel=$(channel)&creator=$(user))`;
+  const nightbotCommand = document.getElementById('nightbotCommand');
+  if (nightbotCommand) {
+    nightbotCommand.textContent = nbCommand;
+  }
+
+  const slCommand = `$readapi(${baseUrl}/api/clips/create?${authParam}&channel=$mychannel&creator=$user)`;
+  const streamlabsCommand = document.getElementById('streamlabsCommand');
+  if (streamlabsCommand) {
+    streamlabsCommand.textContent = slCommand;
+  }
+}
 
 let currentUserId = null;
 let currentUserToken = null;
